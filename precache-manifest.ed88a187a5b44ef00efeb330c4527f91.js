957b8c9fefb14d8a735b3217367a89c0e6cb191a
PoChen Kuo <4082285+PoChenKuo@users.noreply.github.com>
2020-09-03 16:29:27 +0100

    dist

---
+++ precache-manifest.ed88a187a5b44ef00efeb330c4527f91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae1ce2ff062c45080b5",
+    "url": "css/about.caf8ab86.css"
+  },
+  {
+    "revision": "458b3513725ba24e8daa",
+    "url": "css/app.29624427.css"
+  },
+  {
+    "revision": "2c4475c493b1bc25c5e5552144babcff",
+    "url": "img/load.2c4475c4.svg"
+  },
+  {
+    "revision": "dfee8c0b9751593ba7add6124745ed54",
+    "url": "index.html"
+  },
+  {
+    "revision": "dae1ce2ff062c45080b5",
+    "url": "js/about.3e745f15.js"
+  },
+  {
+    "revision": "458b3513725ba24e8daa",
+    "url": "js/app.8871b548.js"
+  },
+  {
+    "revision": "c3025bb6df78a58e5507",
+    "url": "js/chunk-vendors.7cb079c4.js"
+  },
+  {
+    "revision": "2c8ff6bb23b8af1f53ebe19c5fef8dc3",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
